ed25c1d2d6ace3107cdc836c73e21b8eb7676c74
CristhianCotte <cotte372@gmail.com>
2025-02-03 08:19:51 -0500

    first commit

---
+++ Controllers/RegistrosController.cs
@@ -0,0 +1,38 @@
+﻿using CapaEnttidad;
+using CapaNegocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace PracticalQuiz.Controllers
+{
+    public class RegistrosController : Controller
+    {
+        // GET: Registros
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public JsonResult ListarRegistros()
+        {
+            List<SEVECLIE> olista = new List<SEVECLIE> ();
+            olista = new CN_SVECLIE().list();
+            return Json(olista, JsonRequestBehavior.AllowGet);
+        }
+        [HttpGet]
+        public JsonResult ListarEstadoCivil()
+        {
+            List<EstadoCivil> olista = new List<EstadoCivil>();
+            olista = new CN_EstadoCivil().list();
+            return Json(olista, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult Add(SEVECLIE nuevoCliente)
+        {
+            var result = new CN_SVECLIE().Add(nuevoCliente);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
